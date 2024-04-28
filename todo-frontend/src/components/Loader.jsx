aee067cabc66e519dbf881d6cb2ff46bce8b091e
Vaibhav7057 <vaibhavgedamvg.vg@gmail.com>
2024-04-28 13:56:00 +0530

    loader changed

--- todo-frontend/src/components/Loader.jsx
+++ todo-frontend/src/components/Loader.jsx
@@ -4,9 +4,8 @@ const Loader = () => {
   return (
     <div className="bg-[#011015] h-screen w-screen flex justify-center items-center ">
       <div className="relative flex justify-center items-center">
-        <div className="ring"></div>
-        <div className="ring"></div>
-        <div className="ring"></div>
+        <div className="ring w-[200px] h-[200px] "></div>
+        <div className="ring w-[190px] h-[190px] "></div>
         <span className="text-white">...loading</span>
       </div>
     </div>
